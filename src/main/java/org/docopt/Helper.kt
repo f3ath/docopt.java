43d27abe22832789d62c42ef21117e579ce57f17
f3ath <karapetov@gmail.com>
2021-04-18 08:26:16 -0700

    wip

--- src/main/java/org/docopt/Helper.kt
+++ src/main/java/org/docopt/Helper.kt
@@ -1,4 +1,34 @@
 package org.docopt
 
+import java.net.URL
+
 object Helper {
+    fun pureBaseName(url: URL): String {
+        val name = url.path
+        return if (name.isEmpty()) {
+            name
+        } else name
+            .replaceFirst("^.+/".toRegex(), "")
+            .replaceFirst("\\.[^.]+$".toRegex(), "")
+    }
+
+    fun argv(argv: String): List<String>? {
+        val u = Python.list(argv.trim { it <= ' ' }
+            .split("\\s+".toRegex()).toTypedArray())
+        u.removeAt(0)
+        return u
+    }
+
+    fun argv(argv: List<String>): String {
+        val sb = StringBuilder()
+        for (arg in argv) {
+            sb.append("\"")
+            sb.append(arg.replace("\"".toRegex(), "\\\""))
+            sb.append("\" ")
+        }
+        if (argv.isNotEmpty()) {
+            sb.setLength(sb.length - 1)
+        }
+        return sb.toString()
+    }
 }
