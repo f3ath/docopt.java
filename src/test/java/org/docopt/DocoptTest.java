da25dde5068607f8405f6a37a96b63f1201e04b8
f3ath <karapetov@gmail.com>
2021-04-18 11:28:58 -0700

    wip

--- src/test/java/org/docopt/DocoptTest.java
+++ src/test/java/org/docopt/DocoptTest.java
@@ -1,116 +1,98 @@
 package org.docopt;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import org.jetbrains.annotations.NotNull;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.regex.Pattern;
 
 public final class DocoptTest extends TestCase {
 
-    private static final TypeReference<Map<String, Object>> TYPE_REFERENCE =
-            new TypeReference<Map<String, Object>>() {
-            };
-
     public static Test suite() {
         try {
-            final URL url;
+            return getTestSuite();
+        } catch (final IOException e) {
+            throw new AssertionError(e.getMessage());
+        }
+    }
 
-            url = DocoptTest.class.getResource("/testcases.docopt");
+    @NotNull
+    private static TestSuite getTestSuite() throws IOException {
+        final URL url;
 
-            System.out.println("Generating test cases from " + url);
+        url = DocoptTest.class.getResource("/testcases.docopt");
 
-            final String name = Helper.INSTANCE.pureBaseName(url);
+        System.out.println("Generating test cases from " + url);
 
-            String raw = Docopt.read(url.openStream());
+        final String name = Helper.INSTANCE.pureBaseName(url);
 
-            raw = Pattern.compile("#.*$", Pattern.MULTILINE).matcher(raw)
-                    .replaceAll("");
+        String raw = Docopt.read(url.openStream());
 
-            if (raw.startsWith("\"\"\"")) {
-                raw = raw.substring(3);
+        raw = Pattern.compile("#.*$", Pattern.MULTILINE).matcher(raw)
+                .replaceAll("");
+
+        if (raw.startsWith("\"\"\"")) {
+            raw = raw.substring(3);
+        }
+
+        int index = 0;
+
+        final TestSuite suite = new TestSuite("docopt");
+        final List<DocoptTest> tests = new ArrayList<>();
+
+        for (final String fixture : raw.split("r\"\"\"")) {
+            if (fixture.isEmpty()) {
+                continue;
             }
 
-            int index = 0;
+            final String doc1;
+            final String body;
 
-            final TestSuite suite = new TestSuite("docopt");
+            {
+                final String[] u = Py.INSTANCE.partition(fixture, "\"\"\"");
+                doc1 = u[0];
+                body = u[2];
+            }
 
-            for (final String fixture : raw.split("r\"\"\"")) {
-                if (fixture.isEmpty()) {
+            boolean first = true;
+
+            for (final String _case : body.split("\\$")) {
+                if (first) {
+                    first = false;
                     continue;
                 }
 
-                final String doc1;
-                final String body;
+                final String argv1;
+                final String expect;
 
                 {
-                    final String[] u = Py.INSTANCE.partition(fixture, "\"\"\"");
-                    doc1 = u[0];
-                    body = u[2];
+                    final String[] u = Py.INSTANCE.partition(_case.trim(), "\n");
+                    argv1 = u[0];
+                    expect = u[2];
                 }
 
-                boolean first = true;
+                DocoptTest test = new DocoptTest(String.format("%s_%d", name,
+                        ++index), doc1, Helper.INSTANCE.argv(argv1), Helper.INSTANCE.expect(expect));
 
-                for (final String _case : body.split("\\$")) {
-                    if (first) {
-                        first = false;
-                        continue;
-                    }
-
-                    final String argv1;
-                    final String expect;
-
-                    {
-                        final String[] u = Py.INSTANCE.partition(_case.trim(), "\n");
-                        argv1 = u[0];
-                        expect = u[2];
-                    }
-
-                    suite.addTest(new DocoptTest(String.format("%s_%d", name,
-                            ++index), doc1, Helper.INSTANCE.argv(argv1), expect(expect)));
-                }
+                suite.addTest(test);
             }
-
-            return suite;
-        } catch (final IOException e) {
-            final String message;
-
-            if (e instanceof FileNotFoundException) {
-                message = "No such file";
-            } else {
-                message = e.getMessage();
-            }
-
-            throw new AssertionError(message);
         }
+
+        return suite;
     }
 
-    private static Object expect(final String expect) {
-        if ("\"user-error\"".equals(expect)) {
-            return "\"user-error\"";
-        }
-
-        try {
-            return new ObjectMapper().readValue(expect, TYPE_REFERENCE);
-        } catch (final IOException e) {
-            throw new IllegalStateException(
-                    "could not parse JSON object from:\n" + expect, e);
-        }
-    }
 
     private final String doc;
     private final List<String> argv;
     private final Object expected;
 
-    private DocoptTest(final String name, final String doc,
-                       final List<String> argv, final Object expected) {
+    DocoptTest(final String name, final String doc,
+               final List<String> argv, final Object expected) {
         super(name);
 
         this.doc = doc;
@@ -121,10 +103,12 @@ public final class DocoptTest extends TestCase {
     @Override
     protected void runTest() {
         Object actual;
-
         try {
-            actual = new Docopt(doc).withStdOut(null).withStdErr(null)
-                    .withExit(false).parse(argv);
+            actual = new Docopt(doc)
+                    .withStdOut(null)
+                    .withStdErr(null)
+                    .withExit(false)
+                    .parse(argv);
         } catch (final DocoptExitException e) {
             actual = "\"user-error\"";
         }
--- src/test/java/org/docopt/Helper.kt
+++ src/test/java/org/docopt/Helper.kt
@@ -1,5 +1,8 @@
 package org.docopt
 
+import com.fasterxml.jackson.core.type.TypeReference
+import com.fasterxml.jackson.databind.ObjectMapper
+import java.io.IOException
 import java.net.URL
 
 object Helper {
@@ -31,4 +34,19 @@ object Helper {
         }
         return sb.toString()
     }
+
+    val types: TypeReference<Map<String, Any>> =
+        object : TypeReference<Map<String, Any>>() {}
+
+    fun expect(expect: String): Any? = if ("\"user-error\"" == expect) {
+        "\"user-error\""
+    } else try {
+        ObjectMapper().readValue(expect, types)
+    } catch (e: IOException) {
+        throw IllegalStateException(
+            "could not parse JSON object from:\n$expect", e
+        )
+    }
+
+
 }
