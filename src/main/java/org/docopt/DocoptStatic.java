c99eb6b7cfbd5ae8c145e4b0799314c3c3275fc0
f3ath <karapetov@gmail.com>
2021-04-18 16:05:37 -0700

    wip

--- src/main/java/org/docopt/DocoptStatic.java
+++ src/main/java/org/docopt/DocoptStatic.java
@@ -6,6 +6,205 @@ import java.util.Scanner;
 
 public class DocoptStatic {
 
+    static Required parsePattern(final String source,
+                                 final List<Option> options) {
+        String source1 = source;
+        source1 = Py.Re.INSTANCE.sub("([\\[\\]\\(\\)\\|]|\\.\\.\\.)", " $1 ", source1);
+        List<String> $source;
+        {
+            $source = Py.INSTANCE.list();
+
+
+            for (final String s : Py.Re.INSTANCE.split("\\s+|(\\S*<.*?>)", source1)) {
+                if (s != null && !s.equals("")) {
+                    $source.add(s);
+                }
+            }
+        }
+
+        final Tokens tokens = new Tokens($source, DocoptLanguageError.class);
+        final List<? extends Pattern> result = parseExpr(tokens, options);
+
+        if (tokens.current() != null) {
+            throw tokens.error("unexpected ending: %s", Py.INSTANCE.join(" ", tokens));
+        }
+
+        return new Required(result);
+    }
+
+    static List<LeafPattern> parseArgv(final Tokens tokens,
+                                       final List<Option> options, final boolean optionsFirst) {
+        final List<LeafPattern> parsed = Py.INSTANCE.list();
+
+        while (tokens.current() != null) {
+            if ("--".equals(tokens.current())) {
+                {
+                    for (final String v : tokens) {
+                        parsed.add(new Argument(null, v));
+                    }
+
+                    return parsed;
+                }
+            }
+
+            // TODO: Why don't we check for tokens.current != "--" here?
+            if (tokens.current().startsWith("--")) {
+                parsed.addAll(parseLong(tokens, options));
+            } else if (tokens.current().startsWith("-")
+                    && !"-".equals(tokens.current())) {
+                parsed.addAll(parseShorts(tokens, options));
+            } else if (optionsFirst) {
+                {
+                    for (final String v : tokens) {
+                        parsed.add(new Argument(null, v));
+                    }
+
+                    return parsed;
+                }
+            } else {
+                parsed.add(new Argument(null, tokens.move()));
+            }
+        }
+
+        return parsed;
+    }
+
+    static String read(final InputStream stream) {
+        try (Scanner scanner = new Scanner(stream)) {
+            scanner.useDelimiter("\\A");
+            return scanner.hasNext() ? scanner.next() : "";
+        }
+    }
+
+    static List<String> parseSection(final String name,
+                                     final String source) {
+        {
+            final List<String> u = Py.Re.INSTANCE.findAll("^([^\\n]*" + name +
+                    "[^\\n]*\\n?(?:[ \\t].*?(?:\\n|$))*)", source, Py.Re.IGNORECASE | Py.Re.MULTILINE);
+
+            for (int i = 0; i < u.size(); i++) {
+                u.set(i, u.get(i).trim());
+            }
+
+            return u;
+        }
+    }
+
+    static List<Option> parseDefaults(final String doc) {
+        final List<Option> defaults = Py.INSTANCE.list();
+
+        for (String s : parseSection("options:", doc)) {
+            {
+                final String[] u = Py.INSTANCE.partition(s, ":");
+                s = u[2];
+            }
+
+            List<String> split;
+
+            {
+                split = Py.Re.INSTANCE.split("\\n *(-\\S+?)", "\n" + s);
+                split.remove(0);
+            }
+
+            {
+                final List<String> u = Py.INSTANCE.list();
+
+                for (int i = 1; i < split.size(); i += 2) {
+                    u.add(split.get(i - 1) + split.get(i));
+                }
+
+                split = u;
+            }
+
+            {
+                for (final String $s : split) {
+                    if ($s.startsWith("-")) {
+                        defaults.add(Option.parse($s));
+                    }
+                }
+            }
+        }
+
+        return defaults;
+    }
+
+    static String formalUsage(String section) {
+        {
+            final String[] u = Py.INSTANCE.partition(section, ":");
+            section = u[2];
+        }
+
+        final List<String> pu = Py.INSTANCE.split(section);
+
+        {
+            final StringBuilder sb = new StringBuilder();
+
+            sb.append("( ");
+
+            final String u = pu.remove(0);
+
+            if (!pu.isEmpty()) {
+                for (final String s : pu) {
+                    if (s.equals(u)) {
+                        sb.append(") | (");
+                    } else {
+                        sb.append(s);
+                    }
+
+                    sb.append(" ");
+                }
+
+                sb.setLength(sb.length() - 1);
+            }
+
+            sb.append(" )");
+
+            return sb.toString();
+        }
+    }
+
+    static void extras(final boolean help, final String version,
+                       final List<? extends LeafPattern> options, final String doc) {
+        boolean u;
+
+        {
+            u = false;
+
+            if (help) {
+                for (final LeafPattern o : options) {
+                    if ("-h".equals(o.getName()) | "--help".equals(o.getName())) {
+                        if (Py.INSTANCE.bool(o.getValue())) {
+                            u = true;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        if (u) {
+            throw new DocoptExitException(0, doc.replaceAll("^\\n+|\\n+$", ""),
+                    false);
+        }
+
+        {
+            u = false;
+
+            if (version != null && !version.equals("")) {
+                for (final LeafPattern o : options) {
+                    if ("--version".equals(o.getName())) {
+                        u = true;
+                        break;
+                    }
+                }
+            }
+        }
+
+        if (u) {
+            throw new DocoptExitException(0, version, false);
+        }
+    }
+
     private static List<Option> parseLong(final Tokens tokens,
                                           final List<Option> options) {
 
@@ -109,11 +308,99 @@ public class DocoptStatic {
         return Py.INSTANCE.list(o);
     }
 
-    /**
-     * <pre>
-     * shorts ::= '-' ( chars )* [ [ ' ' ] chars ] ;
-     * </pre>
-     */
+    private static List<? extends Pattern> parseExpr(final Tokens tokens,
+                                                     final List<Option> options) {
+        List<Pattern> seq = parseSeq(tokens, options);
+
+        if (!"|".equals(tokens.current())) {
+            return seq;
+        }
+
+        final List<Pattern> result = (seq.size() > 1) ? Py.INSTANCE.list((Pattern) new Required(
+                seq))
+                : seq;
+
+        while ("|".equals(tokens.current())) {
+            tokens.move();
+            seq = parseSeq(tokens, options);
+            result.addAll((seq.size() > 1) ? Py.INSTANCE.list(new Required(seq)) : seq);
+        }
+
+        return (result.size() > 1) ? Py.INSTANCE.list(new Either(result)) : result;
+    }
+
+    private static List<Pattern> parseSeq(final Tokens tokens,
+                                          final List<Option> options) {
+        final List<Pattern> result = Py.INSTANCE.list();
+
+
+        while (!Py.INSTANCE.in(tokens.current(), null, "]", ")", "|")) {
+            List<? extends Pattern> atom = parseAtom(tokens, options);
+
+            if ("...".equals(tokens.current())) {
+                atom = Py.INSTANCE.list(new OneOrMore(atom));
+                tokens.move();
+            }
+
+            result.addAll(atom);
+        }
+
+        return result;
+    }
+
+    private static List<? extends Pattern> parseAtom(final Tokens tokens,
+                                                     final List<Option> options) {
+        final String token = tokens.current();
+
+        List<Pattern> result;
+
+        if ("(".equals(token) || "[".equals(token)) {
+            tokens.move();
+
+            String matching;
+
+            {
+                final List<? extends Pattern> u = parseExpr(tokens, options);
+
+                if ("(".equals(token)) {
+                    matching = ")";
+                    result = Py.INSTANCE.list(new Required(u));
+                } else if ("[".equals(token)) {
+                    matching = "]";
+                    result = Py.INSTANCE.list(new Optional(u));
+                } else {
+                    throw new IllegalStateException();
+                }
+            }
+
+            if (!matching.equals(tokens.move())) {
+                throw tokens.error("unmatched '%s'", token);
+            }
+
+            return Py.INSTANCE.list(result);
+        }
+
+        if ("options".equals(token)) {
+            tokens.move();
+            return Py.INSTANCE.list(new OptionsShortcut());
+        }
+
+        assert token != null;
+        if (token.startsWith("--") && !"--".equals(token)) {
+            return parseLong(tokens, options);
+        }
+
+        if (token.startsWith("-") && !("-".equals(token) || "--".equals(token))) {
+            return parseShorts(tokens, options);
+        }
+
+        if ((token.startsWith("<") && token.endsWith(">")) || Py.INSTANCE.isUpper(token)) {
+            return Py.INSTANCE.list(new Argument(tokens.move()));
+        }
+
+        return Py.INSTANCE.list(new Command(tokens.move()));
+    }
+
     private static List<Option> parseShorts(final Tokens tokens,
                                             final List<Option> options) {
         final String token = tokens.move();
@@ -189,298 +476,4 @@ public class DocoptStatic {
 
         return parsed;
     }
-
-     static Required parsePattern(final String source,
-                                         final List<Option> options) {
-        String source1 = source;
-        source1 = Py.Re.INSTANCE.sub("([\\[\\]\\(\\)\\|]|\\.\\.\\.)", " $1 ", source1);
-        List<String> $source;
-        {
-            $source = Py.INSTANCE.list();
-
-
-            for (final String s : Py.Re.INSTANCE.split("\\s+|(\\S*<.*?>)", source1)) {
-                if (s != null && !s.equals("")) {
-                    $source.add(s);
-                }
-            }
-        }
-
-        final Tokens tokens = new Tokens($source, DocoptLanguageError.class);
-        final List<? extends Pattern> result = parseExpr(tokens, options);
-
-        if (tokens.current() != null) {
-            throw tokens.error("unexpected ending: %s", Py.INSTANCE.join(" ", tokens));
-        }
-
-        return new Required(result);
-    }
-
-    private static List<? extends Pattern> parseExpr(final Tokens tokens,
-                                                     final List<Option> options) {
-        List<Pattern> seq = parseSeq(tokens, options);
-
-        if (!"|".equals(tokens.current())) {
-            return seq;
-        }
-
-        final List<Pattern> result = (seq.size() > 1) ? Py.INSTANCE.list((Pattern) new Required(
-                seq))
-                : seq;
-
-        while ("|".equals(tokens.current())) {
-            tokens.move();
-            seq = parseSeq(tokens, options);
-            result.addAll((seq.size() > 1) ? Py.INSTANCE.list(new Required(seq)) : seq);
-        }
-
-        return (result.size() > 1) ? Py.INSTANCE.list(new Either(result)) : result;
-    }
-
-    private static List<Pattern> parseSeq(final Tokens tokens,
-                                          final List<Option> options) {
-        final List<Pattern> result = Py.INSTANCE.list();
-
-
-        while (!Py.INSTANCE.in(tokens.current(), null, "]", ")", "|")) {
-            List<? extends Pattern> atom = parseAtom(tokens, options);
-
-            if ("...".equals(tokens.current())) {
-                atom = Py.INSTANCE.list(new OneOrMore(atom));
-                tokens.move();
-            }
-
-            result.addAll(atom);
-        }
-
-        return result;
-    }
-
-    private static List<? extends Pattern> parseAtom(final Tokens tokens,
-                                                     final List<Option> options) {
-        final String token = tokens.current();
-
-        List<Pattern> result = Py.INSTANCE.list();
-
-        if ("(".equals(token) || "[".equals(token)) {
-            tokens.move();
-
-            String matching;
-
-            {
-                final List<? extends Pattern> u = parseExpr(tokens, options);
-
-                if ("(".equals(token)) {
-                    matching = ")";
-                    result = Py.INSTANCE.list((Pattern) new Required(u));
-                } else if ("[".equals(token)) {
-                    matching = "]";
-                    result = Py.INSTANCE.list((Pattern) new Optional(u));
-                } else {
-                    throw new IllegalStateException();
-                }
-            }
-
-            if (!matching.equals(tokens.move())) {
-                throw tokens.error("unmatched '%s'", token);
-            }
-
-            return Py.INSTANCE.list(result);
-        }
-
-        if ("options".equals(token)) {
-            tokens.move();
-            return Py.INSTANCE.list(new OptionsShortcut());
-        }
-
-        if (token.startsWith("--") && !"--".equals(token)) {
-            return parseLong(tokens, options);
-        }
-
-        if (token.startsWith("-") && !("-".equals(token) || "--".equals(token))) {
-            return parseShorts(tokens, options);
-        }
-
-        if ((token.startsWith("<") && token.endsWith(">")) || Py.INSTANCE.isUpper(token)) {
-            return Py.INSTANCE.list(new Argument(tokens.move()));
-        }
-
-        return Py.INSTANCE.list(new Command(tokens.move()));
-    }
-
-     static List<LeafPattern> parseArgv(final Tokens tokens,
-                                               final List<Option> options, final boolean optionsFirst) {
-        final List<LeafPattern> parsed = Py.INSTANCE.list();
-
-        while (tokens.current() != null) {
-            if ("--".equals(tokens.current())) {
-                {
-                    for (final String v : tokens) {
-                        parsed.add(new Argument(null, v));
-                    }
-
-                    return parsed;
-                }
-            }
-
-            // TODO: Why don't we check for tokens.current != "--" here?
-            if (tokens.current().startsWith("--")) {
-                parsed.addAll(parseLong(tokens, options));
-            } else if (tokens.current().startsWith("-")
-                    && !"-".equals(tokens.current())) {
-                parsed.addAll(parseShorts(tokens, options));
-            } else if (optionsFirst) {
-                {
-                    for (final String v : tokens) {
-                        parsed.add(new Argument(null, v));
-                    }
-
-                    return parsed;
-                }
-            } else {
-                parsed.add(new Argument(null, tokens.move()));
-            }
-        }
-
-        return parsed;
-    }
-
-     static List<Option> parseDefaults(final String doc) {
-        final List<Option> defaults = Py.INSTANCE.list();
-
-        for (String s : parseSection("options:", doc)) {
-            {
-                final String[] u = Py.INSTANCE.partition(s, ":");
-                s = u[2];
-            }
-
-            List<String> split;
-
-            {
-                split = Py.Re.INSTANCE.split("\\n *(-\\S+?)", "\n" + s);
-                split.remove(0);
-            }
-
-            {
-                final List<String> u = Py.INSTANCE.list();
-
-                for (int i = 1; i < split.size(); i += 2) {
-                    u.add(split.get(i - 1) + split.get(i));
-                }
-
-                split = u;
-            }
-
-            {
-                for (final String $s : split) {
-                    if ($s.startsWith("-")) {
-                        defaults.add(Option.parse($s));
-                    }
-                }
-            }
-        }
-
-        return defaults;
-    }
-
-     static List<String> parseSection(final String name,
-                                             final String source) {
-        {
-            final List<String> u = Py.Re.INSTANCE.findAll("^([^\\n]*" + name +
-                    "[^\\n]*\\n?(?:[ \\t].*?(?:\\n|$))*)", source, Py.Re.IGNORECASE | Py.Re.MULTILINE);
-
-            for (int i = 0; i < u.size(); i++) {
-                u.set(i, u.get(i).trim());
-            }
-
-            return u;
-        }
-    }
-
-     static String formalUsage(String section) {
-        {
-            final String[] u = Py.INSTANCE.partition(section, ":");
-            section = u[2];
-        }
-
-        final List<String> pu = Py.INSTANCE.split(section);
-
-        {
-            final StringBuilder sb = new StringBuilder();
-
-            sb.append("( ");
-
-            final String u = pu.remove(0);
-
-            if (!pu.isEmpty()) {
-                for (final String s : pu) {
-                    if (s.equals(u)) {
-                        sb.append(") | (");
-                    } else {
-                        sb.append(s);
-                    }
-
-                    sb.append(" ");
-                }
-
-                sb.setLength(sb.length() - 1);
-            }
-
-            sb.append(" )");
-
-            return sb.toString();
-        }
-    }
-
-     static void extras(final boolean help, final String version,
-                               final List<? extends LeafPattern> options, final String doc) {
-        boolean u;
-
-        {
-            u = false;
-
-            if (help) {
-                for (final LeafPattern o : options) {
-                    if ("-h".equals(o.getName()) | "--help".equals(o.getName())) {
-                        if (Py.INSTANCE.bool(o.getValue())) {
-                            u = true;
-                            break;
-                        }
-                    }
-                }
-            }
-        }
-
-        if (u) {
-            throw new DocoptExitException(0, doc.replaceAll("^\\n+|\\n+$", ""),
-                    false);
-        }
-
-        {
-            u = false;
-
-            if (version!= null && !version.equals("")) {
-                for (final LeafPattern o : options) {
-                    if ("--version".equals(o.getName())) {
-                        u = true;
-                        break;
-                    }
-                }
-            }
-        }
-
-        if (u) {
-            throw new DocoptExitException(0, version, false);
-        }
-    }
-
-    static String read(final InputStream stream) {
-
-        try (Scanner scanner = new Scanner(stream)) {
-            scanner.useDelimiter("\\A");
-            return scanner.hasNext() ? scanner.next() : "";
-        }
-    }
-
-
 }
