e00791bbd3a49cf24fcb6ac33edee9940a764b3b
f3ath <karapetov@gmail.com>
2021-05-01 11:04:56 -0700

    refactor

--- src/main/kotlin/org/docopt/Command.kt
+++ src/main/kotlin/org/docopt/Command.kt
@@ -2,16 +2,12 @@ package org.docopt
 
 internal class Command constructor(name: String?, value: Any? = false) :
     Argument(name, value) {
-    override fun singleMatch(left: List<LeafPattern>): SingleMatchResult {
-        for (n in left.indices) {
-            val pattern = left[n]
-            if (pattern is Argument) {
-                if (name == pattern.value) {
-                    return SingleMatchResult(n, Command(name, true))
-                }
-                break
-            }
+    override fun singleMatch(left: List<LeafPattern>): SingleMatchResult = left
+        .indexOfFirst { it is Argument && it.value == name }
+        .let {
+            if (it > -1)
+                SingleMatchResult(it, Command(name, true))
+            else
+                SingleMatchResult()
         }
-        return SingleMatchResult()
-    }
 }
